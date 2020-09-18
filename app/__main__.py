3fdb4d766cf11a80cf844360c8511e819784bf2a
shind <shind@sfu.ca>
2020-09-18 16:39:31 -0700

    add rushing jobs to the pipeline

--- app/__main__.py
+++ app/__main__.py
@@ -1,14 +1,19 @@
 """https://github.com/guga31bb/nflfastR-data"""
 from app.config import configure_logging
 from app.jobs import (
+    rushing_by_player,
     play_by_play,
-    roster,
+    roster
 )
 
 
 def main():
+    # Raw Data
     play_by_play.run()
     roster.run()
+
+    # Derived Data
+    rushing_by_player.run()
 
 
 if __name__ == "__main__":
